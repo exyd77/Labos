001a239ce6ca44fc3f86bb7bdc30350fedf5a6c0
Kevin Couture <kevincouture@hotmail.com>
2021-10-10 09:30:18 -0400

    Ajout de la classe FizzBuzz

---
+++ src/FizzBuzz.java
@@ -0,0 +1,26 @@
+package Labos.src;
+
+public class FizzBuzz {
+
+    final static int MAX = 100;
+
+    public static void main(String[] args) {
+        int counter = 1;
+
+        while (counter <= MAX) {
+
+            if ((counter % 3 == 0) && (counter % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (counter % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (counter % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(counter);
+            }
+
+            counter++;
+        }
+
+    }
+}
